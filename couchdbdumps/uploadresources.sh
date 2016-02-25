7a613171e4ef76e1aa4789f31c0236caf3407087
cmorandi <kryzm@yahoo.com>
2016-02-25 16:15:16 +0000

    fixed document upload issues due to hostname

--- couchdbdumps/uploadresources.sh
+++ couchdbdumps/uploadresources.sh
@@ -1,3 +1,3 @@
 for f in ./testresources/*.json; do
-  cat ./testresources/start $f ./testresources/end | curl --trace - -X POST -d @- http://admin:password@192.168.99.100:8002/publish --header "Content-Type:application/json" 
+  cat ./testresources/start $f ./testresources/end | curl --trace - -X POST -d @- http://admin:password@learningregistry/publish --header "Content-Type:application/json" 
 done
